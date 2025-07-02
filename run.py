68922ee09780171fcae920601d2e647749cff37b
Jada Brown <jbrowbb@siue.edu>
2025-07-02 17:43:19 -0500

    all files up

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
+import urllib.parse
+import requests
 import os
+from module_1.DownloadHelper import DownLoadHelper
+from module_1.download_website import DownloadJob
+from module_2.gemini import GeminiGenerateJobs
+from module_2.gemini import GeminiGenerateResume
 from module_1.pdf_extractor import PDFExtractor
 from module_2.file_handler import FileHandler
 
@@ -35,3 +41,49 @@ if __name__ =="__main__":
             print(f"Successfully extracted text from 'Resume.pdf' and saved to '{txt_output_path}'.")
         else:
             print(f"Failed to save extracted text to '{txt_output_path}'. Check console for errors.")
+
+
+    file_path = 'Jobs/urls.txt'
+    #create a object of DownloadArticle
+    url_reader = DownloadJob()
+
+    try: 
+        with open(file_path, 'r') as file:
+            urls = file.readlines() #read every url
+            urls = [url.strip() for url in urls]    # strips whitespace
+
+        # Check if the list is empty after stripping whitespace
+        if not urls:
+            print(f"Error: No URLs found in '{file_path}'. The file might be empty or contain only whitespace.")
+            exit()
+    except FileNotFoundError as e:
+        # Handle the error if the input file for urls is not found
+        error_message = f"Error: {e.filename} not found."
+        print(error_message)
+        exit()
+            
+    for index, url in enumerate(filtered_urls):
+        
+        url_reader.download_articles_from_file(url,index)
+
+        # use the outputed file to generate the summary
+        input_path = f"Job/General/job_{index+1}.txt" 
+        try: 
+            with open(input_path, "r") as file:
+                lines = file.readlines()
+                article_content = "".join(lines[0:]) if len(lines) > 1 else ""
+        except FileNotFoundError as e:
+            # Handle the error if the input file for article is not found
+            error_message = f"Error: {e.filename} not found."
+            print(error_message)
+            exit()
+        if not article_content:
+            print("Error: No content found in input file.")
+
+        # Generate the sumary for present article
+        summary_generator = GeminiGenerateJobs()
+        GeminiGenerateJobs.configure_genai_with_api_key()
+        concise_job = summary_generator.generate_concise_job(job_content)
+
+        title = summary_generator.generate_spicy_title(concise_job)
+        summary_generator.save_job(title, concise_job,index)
--- test.py
+++ test.py
@@ -39,3 +39,86 @@ if __name__ == "__main__":
             print(f"Successfully extracted text from 'Resume.pdf' and saved to '{txt_output_path}'.")
         else:
             print(f"Failed to save extracted text to '{txt_output_path}'. Check console for errors.")
+
+
+
+
+import urllib.parse
+import requests
+import os
+
+from module_1.DownLoadHelper import DownLoadHelper
+from module_1.DownloadArticles import DownloadArticle
+from module_2.Gemini import GeminiGenerate
+from module_3.testcase import testcase
+
+
+if __name__ == "__main__":
+    # Example usage: Download articles from URLs in a text file
+    file_path = 'Data/raw/raw_urls.txt'
+    #create a object of DownloadArticle
+    url_reader = DownloadArticle()
+
+    #Through the urls to output two file for each articles.
+    try: 
+        with open(file_path, 'r') as file:
+            urls = file.readlines() #read every url
+            urls = [url.strip() for url in urls]    # strips whitespace
+
+        # Check if the list is empty after stripping whitespace
+        if not urls:
+            print(f"Error: No URLs found in '{file_path}'. The file might be empty or contain only whitespace.")
+            exit()
+    except FileNotFoundError as e:
+        # Handle the error if the input file for urls is not found
+        error_message = f"Error: {e.filename} not found."
+        print(error_message)
+        exit()
+    
+    Testcase = testcase()
+    # filters out homepage/index pages using function
+    filtered_urls = Testcase.remove_home_idx_links(urls)
+            
+    for index, url in enumerate(filtered_urls):
+        #print(f"Downloading article from URL: {url}")
+        url_reader.download_articles_from_file(url,index) #download the article
+
+        # use the outputed file to generate the summary
+        input_path = f"Data/processed/DownLoadFile/article_{index+1}.txt" 
+        try: 
+            with open(input_path, "r") as file:
+                lines = file.readlines()
+                article_content = "".join(lines[0:]) if len(lines) > 1 else ""
+        except FileNotFoundError as e:
+            # Handle the error if the input file for article is not found
+            error_message = f"Error: {e.filename} not found."
+            print(error_message)
+            exit()
+        if not article_content:
+            print("Error: No content found in input file.")
+
+        # Generate the sumary for present article
+        summary_generator = GeminiGenerate()
+        GeminiGenerate.configure_genai_with_api_key()
+        concise_article = summary_generator.generate_concise_article(article_content)
+
+        # Generate spicy title using the summary if title is not provided
+        title = summary_generator.generate_spicy_title(concise_article)
+        summary_generator.save_article(title, concise_article,index)
+
+    #some test cases in here.
+
+    #The path of the folder that need count number of word.
+    Countwords_folder_path = 'Data/processed/GimiAISummalization'
+    #The path of the folder that need count number of files.
+    Countnum_folder_path = 'Data/processed'
+    #what expected number of files.
+    num_files_expected = 10
+    #test if we got 20 files.
+    Testcase.check_output_files(Countnum_folder_path, num_files_expected)
+    #test if the AI generate the right number of words for summary
+    word_count_dict=Testcase.count_words_in_files(Countwords_folder_path)
+    # Display word count for each file
+    for filename, word_count in word_count_dict.items():
+        print(f"{filename}: {word_count} words")   
+    folder_path = 'Data/processed'
